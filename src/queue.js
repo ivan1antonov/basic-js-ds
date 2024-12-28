f5e6515e213c072de62c543aed24585af4e391b4
Ivan Antonov <ivan1antonov@gmail.com>
2024-12-28 14:49:25 +0400

    feat: add 3 solution task

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor() {
+    this.head = null;
+    this.ended = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.ended) {
+      this.head = newNode;
+      this.ended = newNode;
+    } else {
+      this.ended.next = newNode;
+      this.ended = newNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      throw new Error('queue is empty');
+    }
+
+    let value = this.head.value;
+    this.head = this.head.next;
+
+    if (!this.head) {
+      this.tail = null;
+    }
+
+    return value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) throw new Error(`${l} is not exist`);
+
+  while (l.value === k) {
+    l = l.next;
+  }
+
+  if (!l) return null;
+
+  let prevEl = l;
+  let current = l.next;
+
+  while (current) {
+    if (current.value === k) {
+      prevEl.next = current.next;
+    } else {
+      prevEl = current;
+    }
+    current = current.next;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
